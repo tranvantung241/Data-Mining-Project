51cd49a52ac17023b1b18667d3623781fdb367bf
TranTung <tranvantung241@gmail.com>
2018-11-24 23:45:22 +0700

    check missing data

--- app.py
+++ app.py
@@ -6,15 +6,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import sys   
-from flask import render_template_string
- 
-
+from flask import render_template_string 
+from wtforms import Form, TextField
 
 __author__ = 'TranTung'
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+# class Form(FlaskForm):
+#      column_name = SelectField('column_name', choices[('1','2','3')])
+
+
+
+# =========================================FUNCTION===========================================================
+# read data
+def read_data():
+    data = pd.read_csv("./dataset/2017.csv", encoding ='latin1')  
+    return data
+
+# get data  use]
+def get_data_use():
+    data = read_data()
+    data_use = data[['1st Road Class & No','Road Surface','Lighting Conditions','Weather Conditions','Type of Vehicle', 'Casualty Severity']]
+    return data_use
+
+# hàm gọi các tên column
+def list_columns_name():
+    data_use = get_data_use()
+    list_column_name = data_use.columns 
+    return list_column_name
+
+# get shape data use
+def getShape():
+    data_use = get_data_use()
+    shape = data_use.shape
+    return shape
+
+# khởi tạo dictionary rỗng
+def dic_column_missing():
+    dic_column_missing = {}
+    return dic_column_missing
+
+# =========================================ROUTE===========================================================
+#route home -----------------------------------------------------------------------------------------------
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -23,14 +58,18 @@ def index():
 def home():
     return render_template("home.html")
 
+
+#route describle -----------------------------------------------------------------------------------------------
 @app.route("/describle")
 def describle():
     return render_template("describle.html")
 
+#route upload_file -----------------------------------------------------------------------------------------------
 @app.route("/upload_file")
 def upload_file():
     return render_template("upload.html")
 
+#route upload -----------------------------------------------------------------------------------------------
 @app.route("/upload", methods=['POST', 'GET'])
 def upload(): 
     target = os.path.join(APP_ROOT, 'images/')
@@ -57,22 +96,44 @@ def upload():
     return render_template("complete.html")
 
 
-
+ 
+#route showw data -----------------------------------------------------------------------------------------------
 @app.route('/show')
-def show():
-    data = pd.read_csv("./images/2015.csv", encoding ='latin1')
-    return data.to_html(border=1)
+def show(): 
+    return render_template("show.html", df_view = read_data())
 
-@app.route('/show2')
-def show2():
-    data = pd.read_csv("./images/2015.csv", encoding ='latin1')  
-    value = render_template_string(data.to_html(border=1), context );
-    return render_template("chart.html", value = value)
+#route show data use -----------------------------------------------------------------------------------------------
+@app.route('/show_use')
+def show_use(): 
+    return render_template("show_use.html", df_view = get_data_use())
 
+#route chart -----------------------------------------------------------------------------------------------
 @app.route('/chart')
 def chart():
     return render_template("chart.html")
 
+#route preprocess -----------------------------------------------------------------------------------------------
+@app.route('/preprocess')
+def preprocess():  
+    return render_template("pre_process.html", list_column_name = list_columns_name(),
+                             dic_column_missing= dic_column_missing(),
+                             len=len(list_columns_name()), shape= getShape())
+
+
+#route check_missing -----------------------------------------------------------------------------------------------
+@app.route('/check_missing', methods=['POST'] )
+def check_missing(): 
+    data_use = get_data_use()
+    list_column_name = list_columns_name()
+    dic_column_missing = {}
+    sum_missing = []
+    # nhận dữ liệu từ form submit
+    if request.method == 'POST':
+        for i in range(1, len(list_column_name)):
+            sum = data_use[list_column_name[i]].isnull().sum()
+            dic_column_missing[list_column_name[i]] = sum
+    return render_template('pre_process.html', dic_column_missing = dic_column_missing, 
+        list_column_name = list_columns_name())
 
 if __name__ == "__main__":
-    app.run(host='localhost', debug=True)
+    app.run(host='localhost', port=8080, debug=True)
