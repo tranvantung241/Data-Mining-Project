f169c664b1e452a69a7125b60abd2e75ae4c834e
TranTung <tranvantung241@gmail.com>
2018-12-18 07:03:20 +0700

    thuat toan

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def renew_data(data_handed):
 def get_data_use():
     data = read_data()
     data_use = data[['1st Road Class & No','Road Surface','Lighting Conditions','Weather Conditions','Type of Vehicle', 'Casualty Severity']]
+    # data_use = data.iloc[:, [0,1,2,3, 4, 5]].values
     return data_use
 
 # hàm gọi các tên column
@@ -51,7 +52,7 @@ def dic_column_missing():
     return dic_column_missing
 
 # creat list values converted
-def list_converted(name_colum): 
+def list_converted(name_colum):  
     global data_use
     dict_converted = {}
     values = list(set(data_use[name_colum]))
@@ -62,7 +63,9 @@ def list_converted(name_colum):
         value_replace = value_replace +1
 
     return dict_converted
-
+# reset()
+def reset():
+    pass
 # =========================================DECLARE===========================================================
 aler = "Removed data."
 aler2 = "Kích thước sau khi remove data:"
@@ -72,13 +75,33 @@ dic_column_noise = {}
 shape = getShape();
 accurate_entropy=""
 accurate_gini = ""
+accurate_bayes = ""
 cm_entropy = []
 cm_gini = []
+cm_bayes = []
+filename_converted =""
+
+dict_Road_Class_2 = {} 
+dict_Road_Surface_2  = {} 
+dict_Lighting_2  = {} 
+dict_Weather_2  = {} 
+dict_Vehicle_2  = {} 
+
+classifier_entropy = None
+classifier_gini = None
+classifier_bayes = None
+sc = None
 
 # =========================================ROUTE===========================================================
+ 
 #route home -----------------------------------------------------------------------------------------------
 @app.route("/")
 def index():
+
+    # reset lại data_use
+    global data_use 
+    data_use = get_data_use()
+
     return render_template("home.html")
 
 @app.route("/home")
@@ -142,6 +165,10 @@ def chart():
 #route preprocess -----------------------------------------------------------------------------------------------
 @app.route('/preprocess')
 def preprocess():  
+
+    # reset các giá trị khai báo ban đầu
+    reset()
+
     return render_template("pre_process.html", list_column_name = list_columns_name(),
                              dic_column_missing = dic_column_missing(), 
                              len=len(list_columns_name()), shape= getShape(), dic_column_noise =dic_column_noise)
@@ -278,14 +305,25 @@ def convert_values():
 @app.route('/converted_values', methods=['POST'] )
 def converted_values():
     global data_use
-    
+    global dict_Road_Class_2 
+    global dict_Road_Surface_2
+    global dict_Lighting_2 
+    global dict_Weather_2
+    global dict_Vehicle_2
+        
     dict_Road_Class  = list_converted('1st Road Class & No')
     dict_Road_Surface  = list_converted('Road Surface')
     dict_Lighting  = list_converted('Lighting Conditions')
     dict_Weather  = list_converted('Weather Conditions')
     dict_Vehicle  = list_converted('Type of Vehicle')
  
-
+    # //lưu sang biến khác
+    dict_Road_Class_2  = dict_Road_Class
+    dict_Road_Surface_2  = dict_Road_Surface
+    dict_Lighting_2  = dict_Lighting
+    dict_Weather_2  = dict_Weather
+    dict_Vehicle_2  = dict_Vehicle
+    # kết thúc ham converted dữ liệu trong data_use đã chuyển thành các số
     return render_template('converted_values.html', 
                             data =   data_use.head(),
                             dict_Road_Class = dict_Road_Class,
@@ -296,7 +334,11 @@ def converted_values():
 
 @app.route('/save_file_converted', methods=['POST'] )
 def save_file_converted():
-    global data_use
+    # tới đây data_use đã preprogressed , tiến hành lưu csv đã converted, với tên file là filename_converted
+    global data_use  
+    global filename_converted
+    global dict_Road_Class
+
     dict_Road_Class  = {}
     dict_Road_Surface  = {}
     dict_Lighting  = {}
@@ -305,7 +347,8 @@ def save_file_converted():
     time  = datetime.datetime.now()
     # time_str = str(time)
     time_str = time.strftime('%d_%m_%Y %H_%M_%S')
-    data_use.to_csv("./dataset/2017_preprocessed "+time_str+".csv",header = True, index= False)
+    filename_converted = "./dataset/2017_preprocessed_converted"+time_str+".csv"
+    data_use.to_csv(filename_converted, header = True, index= False)
     return render_template('converted_values.html',  
                             data =   data_use.head(),
                             dict_Road_Class = dict_Road_Class,
@@ -319,7 +362,21 @@ def save_file_converted():
 def classify():  
     global cm_entropy
     global cm_gini
-    return render_template("classification.html", cm_entropy = cm_entropy, cm_gini=cm_gini) 
+    global data_use
+
+    global dict_Road_Class_2 
+    global dict_Road_Surface_2
+    global dict_Lighting_2 
+    global dict_Weather_2
+    global dict_Vehicle_2
+        
+
+    return render_template("classification.html", cm_entropy = cm_entropy, cm_gini=cm_gini, cm_bayes = cm_bayes,
+                            dict_Road_Class_2 = dict_Road_Class_2,
+                            dict_Road_Surface_2= dict_Road_Surface_2,
+                            dict_Lighting_2=dict_Lighting_2,
+                            dict_Weather_2= dict_Weather_2,
+                            dict_Vehicle_2= dict_Vehicle_2) 
 
 # entropy
 @app.route('/entropy', methods = ['POST'])
@@ -327,7 +384,18 @@ def entropy():
     global accurate_entropy
     global cm_entropy
     global cm_gini
-    dataset_entroy = pd.read_csv("./dataset/2017_preprocessed 13_12_2018 23_11_37.csv", encoding ='latin1')  
+    global filename_converted
+    global data_use
+    global classifier_entropy
+    global cm_bayes
+    global sc
+
+    # đọc dữ liệu  filename vừa lưu xúns
+    # có thể dùng data_use lun 
+
+    # dataset_entroy = pd.read_csv("./dataset/2017_preprocessed_converted17_12_2018 14_45_58.csv", encoding ='latin1')  
+
+    dataset_entroy = pd.read_csv(filename_converted, encoding ='latin1')  
 
     # Split data into input and lable output
     X_entropy = dataset_entroy.iloc[:, [0,1,2,3, 4]].values
@@ -361,16 +429,29 @@ def entropy():
     accurate_entropy =  accuracy_score(y_test_entropy, y_pred_entropy)
 
     return render_template("classification.html", accurate_entropy =accurate_entropy,
-                            cm_entropy = cm_entropy, cm_gini=cm_gini)
+                            cm_entropy = cm_entropy, cm_gini=cm_gini, cm_bayes = cm_bayes,
+                            dict_Road_Class_2 = dict_Road_Class_2,
+                            dict_Road_Surface_2= dict_Road_Surface_2,
+                            dict_Lighting_2=dict_Lighting_2,
+                            dict_Weather_2= dict_Weather_2,
+                            dict_Vehicle_2= dict_Vehicle_2)
 
 # Gini
 @app.route('/gini', methods = ['POST'])
 def gini():
     global accurate_entropy
-    global  cm_entropy;
+    global accurate_gini
+    global cm_entropy
+    global cm_gini
+    global cm_bayes
+    global data_use
+    global classifier_gini
+    global sc
 
-    dataset_gini = pd.read_csv("./dataset/2017_preprocessed 13_12_2018 23_11_37.csv", encoding ='latin1')  
+    # dataset_gini = pd.read_csv("./dataset/2017_preprocessed 13_12_2018 23_11_37.csv", encoding ='latin1')  
      # Split data into input and lable output
+
+    dataset_gini = pd.read_csv(filename_converted, encoding ='latin1')  
     X_gini = dataset_gini.iloc[:, [0,1,2,3, 4]].values
     y_gini = dataset_gini.iloc[:, 5].values
 
@@ -401,10 +482,128 @@ def gini():
     accurate_gini =  accuracy_score(y_test_gini, y_pred_gini)
 
     return render_template("classification.html", accurate_gini =accurate_gini, 
-                            accurate_entropy=accurate_entropy,  cm_entropy = cm_entropy,cm_gini =cm_gini)
+                            accurate_entropy=accurate_entropy,  cm_entropy = cm_entropy,cm_gini =cm_gini, cm_bayes = cm_bayes,
+                            dict_Road_Class_2 = dict_Road_Class_2,
+                            dict_Road_Surface_2= dict_Road_Surface_2,
+                            dict_Lighting_2=dict_Lighting_2,
+                            dict_Weather_2= dict_Weather_2,
+                            dict_Vehicle_2= dict_Vehicle_2)
+
+# Bayes
+@app.route('/bayes', methods = ['POST'])
+def bayes():
+    global accurate_entropy
+    global accurate_gini
+    global accurate_bayes
+    global cm_entropy
+    global cm_gini
+    global cm_bayes
+    global data_use
+    global classifier_bayes
+    global sc
+
+    # dataset_bayes = pd.read_csv("./dataset/2017_preprocessed 13_12_2018 23_11_37.csv", encoding ='latin1')  
+     # Split data into input and lable output
+
+    dataset_bayes = pd.read_csv(filename_converted)  
+    X_bayes = dataset_bayes.iloc[:, [0,1,2,3, 4]].values
+    y_bayes = dataset_bayes.iloc[:, 5].values
+
+    # Split dataset train, dataset test
+    from sklearn.cross_validation import train_test_split
+    X_train_bayes, X_test_bayes, y_train_bayes, y_test_bayes = train_test_split(X_bayes, y_bayes, test_size = 0.25, random_state = 0)
+
+    # Fit_transform data
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train_bayes = sc.fit_transform(X_train_bayes)
+    X_test_bayes = sc.transform(X_test_bayes)
+
+    #Build model decision tree 
+    from sklearn.naive_bayes import GaussianNB
+    classifier_bayes = GaussianNB() 
+    classifier_bayes.fit(X_train_bayes, y_train_bayes)
+
+    y_pred_bayes = classifier_bayes.predict(X_test_bayes)
+
+    # confussion matrix
+    from sklearn.metrics import confusion_matrix
+    cm_bayes = confusion_matrix(y_test_bayes, y_pred_bayes)
+    cm_bayes = cm_bayes.tolist()
+
+    # accuracy_score
+    from sklearn.metrics import accuracy_score
+    accurate_bayes =  accuracy_score(y_test_bayes, y_pred_bayes)
+
+    return render_template("classification.html", accurate_gini =accurate_gini, 
+                            accurate_entropy=accurate_entropy,  
+                            accurate_bayes =accurate_bayes,
+                            cm_entropy = cm_entropy,cm_gini =cm_gini, cm_bayes = cm_bayes,
+                            dict_Road_Class_2 = dict_Road_Class_2,
+                            dict_Road_Surface_2= dict_Road_Surface_2,
+                            dict_Lighting_2=dict_Lighting_2,
+                            dict_Weather_2= dict_Weather_2,
+                            dict_Vehicle_2= dict_Vehicle_2)
 
 
+#classified 
+@app.route('/classified', methods = ['POST'])
+def classified():
+    # global accurate_entropy
+    # global accurate_gini
+    global classifier_entropy
+    global classifier_gini
+    global classifier_bayes
+    global sc
 
+    if request.method == 'POST':
+        x_test = [None]*5
+        x_test[0] = request.form['cot1']
+        x_test[1] = request.form['cot2']
+        x_test[2] = request.form['cot3']
+        x_test[3] = request.form['cot4']
+        x_test[4] = request.form['cot5'] 
+
+        import numpy as np
+        x_test_2 = np.array([ x_test[0], x_test[1], x_test[2], x_test[3], x_test[4]]) 
+        x_test_2D = [x_test_2]
+
+
+        # # thử fit_transform với dữ liêu 
+        # dataset_bayes = pd.read_csv(filename_converted)  
+        # X_bayes = dataset_bayes.iloc[:, [0,1,2,3, 4]].values
+        # y_bayes = dataset_bayes.iloc[:, 5].values
+
+        # # Split dataset train, dataset test
+        # from sklearn.cross_validation import train_test_split
+        # X_train_bayes, X_test_bayes, y_train_bayes, y_test_bayes = train_test_split(X_bayes, y_bayes, test_size = 0.25, random_state = 0)
+
+        # # Fit_transform data
+        # from sklearn.preprocessing import StandardScaler
+        # sc = StandardScaler()
+        # X_train_bayes = sc.fit_transform(X_train_bayes)
+        # X_test_bayes = sc.transform(X_test_bayes)
+
+        # transform 
+        x_test_2D_1 = sc.transform(x_test_2D) 
+
+        name = request.form['action']
+
+        if(name == "Entropy"):
+            y_pred = classifier_entropy.predict(x_test_2D_1)
+        elif (name == "Gini"):
+            y_pred = classifier_gini.predict(x_test_2D_1)
+        elif (name == "Bayes"):
+            y_pred = classifier_bayes.predict(x_test_2D_1) 
+
+    return render_template("classification.html", accurate_gini =accurate_gini, 
+                            accurate_entropy=accurate_entropy, accurate_bayes = accurate_bayes,
+                            cm_entropy = cm_entropy,cm_gini =cm_gini, cm_bayes = cm_bayes,
+                            dict_Road_Class_2 = dict_Road_Class_2,
+                            dict_Road_Surface_2= dict_Road_Surface_2,
+                            dict_Lighting_2=dict_Lighting_2,
+                            dict_Weather_2= dict_Weather_2,
+                            dict_Vehicle_2= dict_Vehicle_2, x_test_list=x_test , y_pred = y_pred, name =name)
 
 if __name__ == "__main__":
     app.run(host='localhost', port=8080, debug=True)
